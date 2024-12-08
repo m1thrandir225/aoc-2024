540df09b33d1b415f742c77c2cf0bfeb4beacf51
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-08 12:02:31 +0100

    feat: day7 solution

--- days/day_7/day_7.go
+++ days/day_7/day_7.go
@@ -2,13 +2,13 @@ package day7
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func Day7PartOne(filename string) {
-	table := make(map[int][]int, 0)
 
 	f, err := os.Open(filename)
 	if err != nil {
@@ -16,47 +16,110 @@ func Day7PartOne(filename string) {
 	}
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
+
+	totalSum := 0
+
 	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.Split(line, ": ")
-		value, err := strconv.Atoi(parts[0])
-		if err != nil {
-			panic(err)
-		}
-		possibilityListStr := strings.Split(parts[1], " ")
-		possibilityList := make([]int, 0)
-		for _, item := range possibilityListStr {
-			numberValueItem, err := strconv.Atoi(item)
-			if err != nil {
-				panic(err)
+		numbers, result := parseLine(scanner.Text())
+		operatorCombos := generateOperatorCombinations([]string{"+", "*"}, len(numbers)-1)
+		for _, operators := range operatorCombos {
+			if evaluate(numbers, operators, result) == result {
+				totalSum += result
+				break
 			}
-			possibilityList = append(possibilityList, numberValueItem)
 		}
-		table[value] = possibilityList
 	}
-
-	for target, numbersToCombine := range table {
-		possibleOperators := len(numbersToCombine) - 1
-
-	}
+	fmt.Println(totalSum)
 }
 
-func evaluate(nums []int, operatorLenght int, operator string) int {
-	result := nums[0]
-	for i := 0; i < operatorLenght; i++ {
-		if operator == "+" {
-			result += nums[i+1]
-		} else if operator == "*" {
-			result *= nums[i+1]
+func Day7PartTwo(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	totalSum := 0
+	for scanner.Scan() {
+		numbers, result := parseLine(scanner.Text())
+		operatorCombos := generateOperatorCombinations([]string{"+", "*", "||"}, len(numbers)-1)
+
+		for _, operators := range operatorCombos {
+			if evaluate(numbers, operators, result) == result {
+				totalSum += result
+				break
+			}
+		}
+	}
+	fmt.Println(totalSum)
+}
+
+func parseLine(line string) ([]int, int) {
+	parts := strings.Fields(line)
+	result, _ := strconv.Atoi(strings.TrimSuffix(parts[0], ":"))
+	numbers := make([]int, len(parts)-1)
+	for i, num := range parts[1:] {
+		numbers[i], _ = strconv.Atoi(num)
+	}
+	return numbers, result
+}
+
+func generateOperatorCombinations(operators []string, length int) [][]string {
+	var combinations [][]string
+	var generate func(current []string)
+
+	generate = func(current []string) {
+		if len(current) == length {
+			combinations = append(combinations, append([]string(nil), current...))
+			return
+		}
+
+		for _, operator := range operators {
+			current = append(current, operator)
+			generate(current)
+			current = current[:len(current)-1]
+		}
+	}
+	generate([]string{})
+	return combinations
+
+}
+
+func evaluate(numbers []int, operators []string, solution int) int {
+	result := numbers[0]
+	for i, op := range operators {
+		switch op {
+		case "+":
+			result += numbers[i+1]
+		case "*":
+			result *= numbers[i+1]
+		case "||":
+			result = concatenate(result, numbers[i+1])
+		}
+
+		if result > solution {
+			return -1
 		}
 	}
 	return result
 }
 
-func matchTarget(nums []int, target int, operatorsLenght int, index int, calculated map[int]bool) {
-	if index == len(nums) {
-		result := evaluate(nums, , operator string)
+func pow(base, exponent int) int {
+	result := 1
+	for exponent > 0 {
+		result *= base
+		exponent--
 	}
+	return result
 }
 
-func Day7PartTwo(filename string) {}
+func concatenate(a, b int) int {
+	digits := 0
+	temp := b
+	for temp > 0 {
+		temp /= 10
+		digits++
+	}
+	return a*pow(10, digits) + b
+}
