e53f24538fefd18c40bd71c0698025b52c35a5a1
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-12-09 21:57:37 +0100

    feat: day9 started (will finish tomorrow)

--- days/day_9/day_9.go
+++ days/day_9/day_9.go
@@ -1,4 +1,46 @@
 package day9
 
-func Day9PartOne(filename string) {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Day9PartOne(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(f)
+	array := make([]int, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "")
+		for _, str := range parts {
+			num, _ := strconv.Atoi(str)
+			array = append(array, num)
+		}
+	}
+	filesArray := make([]int, 0)
+	spaceArray := make([]int, 0)
+	for i := 0; i < len(array); i++ {
+		if i%2 == 0 {
+			filesArray = append(filesArray, array[i])
+		} else {
+			spaceArray = append(spaceArray, array[i])
+		}
+	}
+
+	fmt.Println(filesArray)
+	fmt.Println(spaceArray)
+
+}
+
+func makeRow(files, space []int) string {
+	if len(files) != len(space) {
+		return ""
+	}
+}
 func Day9PartTwo(filename string) {}
--- days/day_9/day_9_test.go
+++ days/day_9/day_9_test.go
@@ -1,6 +1,8 @@
 package day9
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestDay9(t *testing.T) {
 	Day9PartOne("day_9.txt")
